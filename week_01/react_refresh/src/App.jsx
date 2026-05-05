e5ea7eb4c2d934b65f83448a4d81a83dcc53b239
lumesh2911-1 <lumesh2911@gmail.com>
2026-05-05 10:17:21 +0530

    Timer things completed.

--- week_01/react_refresh/src/App.jsx
+++ week_01/react_refresh/src/App.jsx
@@ -1,35 +1,28 @@
-// import React, { use, useState } from "react";
-
-// function App() {
-//   const [state, setState] = useState(0);
-//   return (
-//     <div>
-//       {state}
-//       <button onClick={() => setState(state + 1)}>Test</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 
-export default function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  const onToggle = () => {
-    setDarkMode(!darkMode);
+function App() {
+  const [timer, setTimer] = useState(0);
+
+  const increament = () => {
+    setTimer(timer + 1);
+  };
+
+  const decreament = () => {
+    setTimer(timer - 1);
   };
   return (
-    <div
-      style={{
-        height: "500px",
-        width: "500px",
-        backgroundColor: darkMode ? "black" : "white",
-      }}
-    >
-      <h1>{darkMode}</h1>
-
-      <button onClick={onToggle}>Toggle Theme</button>
-    </div>
+    <>
+      <div style={{ display: "flex", gap: 20, fontSize: 26 }}>
+        <div style={{ cursor: "pointer" }} onClick={increament}>
+          ➕
+        </div>
+        <div>{timer}</div>
+        <div style={{ cursor: "pointer" }} onClick={decreament}>
+          ➖
+        </div>
+      </div>
+    </>
   );
 }
+
+export default App;
--- week_01/react_refresh/src/main.jsx
+++ week_01/react_refresh/src/main.jsx
@@ -1,15 +1,8 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import Header from "./components/Header";
-import H1 from "./components/H1";
+import App from "./App";
 
-// const h1 = React.createElement("h1", null, "Test");
-// const p1 = React.createElement("p", null, "Test1");
 createRoot(document.getElementById("root")).render(
   <>
-    <Header />
-    {/* <h1>Test {2 + 2}</h1>
-    <h1>Test1</h1> */}
-    <H1 title="Test" desc="I am testing" />
+    <App />
   </>,
 );
