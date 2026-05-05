66116899297e69fee8aedc49d3948753a95e860f
lumesh2911-1 <lumesh2911@gmail.com>
2026-05-05 10:25:28 +0530

    Light and dark mode task completed.

--- week_01/react_refresh/src/App.jsx
+++ week_01/react_refresh/src/App.jsx
@@ -1,25 +1,41 @@
 import React, { useState } from "react";
 
 function App() {
-  const [timer, setTimer] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
+  // const [timer, setTimer] = useState(0);
 
-  const increament = () => {
-    setTimer(timer + 1);
-  };
+  // const increament = () => {
+  //   setTimer(timer + 1);
+  // };
 
-  const decreament = () => {
-    setTimer(timer - 1);
+  // const decreament = () => {
+  //   setTimer(timer - 1);
+  // };
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
   };
   return (
     <>
-      <div style={{ display: "flex", gap: 20, fontSize: 26 }}>
+      {/* <div style={{ display: "flex", gap: 20, fontSize: 26 }}>
         <div style={{ cursor: "pointer" }} onClick={increament}>
           ➕
         </div>
         <div>{timer}</div>
         <div style={{ cursor: "pointer" }} onClick={decreament}>
           ➖
-        </div>
+        </div> 
+      </div> */}
+      <div
+        style={{
+          height: "400px",
+          width: "400px",
+          border: "1px solid black",
+          padding: "20px",
+          backgroundColor: darkMode === true ? "black" : "white",
+        }}
+      >
+        <button onClick={handleToggle}>Press Me</button>
       </div>
     </>
   );
