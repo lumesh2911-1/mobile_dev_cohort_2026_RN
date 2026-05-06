5d58dce012b8b44ef9e3d085f38f807ed51483ef
lumesh2911-1 <lumesh2911@gmail.com>
2026-05-06 18:18:55 +0530

    Use effect consept completed.

--- week_01/react_refresh/src/App.jsx
+++ week_01/react_refresh/src/App.jsx
@@ -1,43 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  // const [timer, setTimer] = useState(0);
-
-  // const increament = () => {
-  //   setTimer(timer + 1);
-  // };
-
-  // const decreament = () => {
-  //   setTimer(timer - 1);
-  // };
-
-  const handleToggle = () => {
-    setDarkMode(!darkMode);
-  };
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("Welcome");
+  }, [counter, setCounter]);
   return (
-    <>
-      {/* <div style={{ display: "flex", gap: 20, fontSize: 26 }}>
-        <div style={{ cursor: "pointer" }} onClick={increament}>
-          ➕
-        </div>
-        <div>{timer}</div>
-        <div style={{ cursor: "pointer" }} onClick={decreament}>
-          ➖
-        </div> 
-      </div> */}
-      <div
-        style={{
-          height: "400px",
-          width: "400px",
-          border: "1px solid black",
-          padding: "20px",
-          backgroundColor: darkMode === true ? "black" : "white",
-        }}
-      >
-        <button onClick={handleToggle}>Press Me</button>
-      </div>
-    </>
+    <div style={{ display: "flex", gap: 30, fontSize: 24 }}>
+      <div onClick={() => setCounter(counter + 1)}>➕</div>
+      <div>{counter}</div>
+      <div onClick={() => setCounter(counter - 1)}>➖</div>
+    </div>
   );
 }
 
